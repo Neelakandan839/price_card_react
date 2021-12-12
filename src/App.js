a3d6633ccbe4773e243c72fc7642247f6fa27a3b
Neelakandaan839 <neelakandaan839@gmail.com>
2021-12-12 11:41:59 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Plans />
     </div>
   );
+}
+
+function Plans(){
+  const details=[{
+    plan:"FREE",
+    price:"$0",
+    features:[" Single User"," 5GB Storage"," Unlimited Public Projects"," Community Access"," Unlimited Private Projects"," Dedicated Phone Support"," Free Subdomain"," Monthly Status Reports"],
+  },
+  {
+    plan:"PLUS",
+    price:"$9",
+    features:[" 5 Users"," 50GB Storage"," Unlimited Public Projects"," Community Access"," Unlimited Private Projects"," Dedicated Phone Support"," Free Subdomain"," Monthly Status Reports"],
+  },
+  {
+    plan:"PRO",
+    price:"$49",
+    features:[" Unlimited Users"," 150GB Storage"," Unlimited Public Projects"," Community Access"," Unlimited Private Projects"," Dedicated Phone Support"," Unlimited Free Subdomain"," Monthly Status Reports"],
+  }]
+  return (
+    <><h2>BOOTSTRAP PRICING CARD</h2>
+    <div className="container">
+      {details.map((data,index) => (<Card key={index} name={data.plan} price={data.price} features={data.features} />))}
+    </div></>
+  )
+}
+
+function Card(props){
+  return (
+   <div className="card">
+     <h5>{props.name}</h5>
+     <h1>{props.price}<span>/month</span></h1>
+     <hr></hr>
+     <ul className="ul1">
+     {props.features.map((feature,index)=>(
+       <li key={index}>{feature}</li>
+     ))}
+     </ul>
+     <button>BUY</button>
+   </div>
+  )
 }
 
 export default App;
